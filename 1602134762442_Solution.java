739ee910d17d516df4ac4bf56ad3699a54c8a76f
Dhanush <32243534+dhanush614@users.noreply.github.com>
2020-10-15 10:42:11 +0530

    Update 1602134762442_Solution.java

--- 1602134762442_Solution.java
+++ 1602134762442_Solution.java
@@ -1,115 +1,105 @@
+import java.util.Arrays;
 import java.util.Scanner;
-public class Solution
-{
 
- public static void main(String[] args)
- {
-  //code to read values 
-  Football[] fball=new Football[4];
-  Scanner sc=new Scanner(System.in);
-  for(int i=0;i<fball.length;i++){
-      int playerId=sc.nextInt(); sc.nextLine();
-      String playerName=sc.nextLine();
-      String country=sc.nextLine();
-      int goals=sc.nextInt();
-      fball[i]=new Football(playerId, playerName, country, goals);
-  }
-  String country=sc.nextLine();
-  String country1=sc.nextLine();
-  
-  //code to call required method
-  int sum=findGoalsforCountry(fball, country);
-      if(sum>0){
-          System.out.println(sum);
-      }
-      else{
-      System.out.println("No valid Country specified");
-      }
- }
-  
-  Football fb=getGoalsInAscendingOrder(fball, country);
-  
-  if(fb != null){
-      System.out.println(fb.getplayerId()+"#"+fb.getplayerName()+"#"+fb.getcountry()+"#"+fb.getgoals());
-  }
-  else{
-      System.out.println("Country specified is wrong");
-  }
-  
-   
- 
+public class Solution {
 
-public static int  findGoalsforCountry(Football[] fball,String country)
-  {
-   //method logic
-   int sum=0;
-   for(int i=0;i<fball.length;i++){
-       if(fball[i].getcountry().equalsIgnoreCase(country)){
-           sum=sum+fball[i].getgoals();
-           sum++;
-       }
-   }
-   if(sum==0){
-       return 0;
-   }
-   
-   
-  }
-public static Football[] getGoalsInAscendingOrder(Football[] fball, String country)
-  {
-   //method logic
-   Football[] fb=null;
-   for(int i =0;i<fball.length;i++){
-       if(fball[i].getcountry()==country){
-           fb=fball[i];
-           break;
-       }
-       else{
-           return fb;
-       }
-   }
-   
-  }
+	public static Book[] booksWithPriceRange(double minPrice, double maxPrice, Book[] books) {
+		Book[] result = new Book[2];
+		int j = 0;
+		for (int i = 0; i < books.length; i++) {
+			if (books[i].getPrice() >= minPrice && books[i].getPrice() <= maxPrice) {
+				result[j] = books[i];
+				j++;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int id;
+		String title, author;
+		double price, minPrice, maxPrice;
+		Book[] books = new Book[4];
+		for (int i = 0; i < 4; i++) {
+			id = sc.nextInt();
+			title = sc.next();
+			author = sc.next();
+			price = sc.nextDouble();
+			books[i] = new Book(id, title, author, price);
+		}
+		minPrice = sc.nextDouble();
+		maxPrice = sc.nextDouble();
+		Arrays.sort(books);
+		Book[] result = booksWithPriceRange(minPrice, maxPrice, books);
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i].getId());
+		}
+	}
 }
-class Football
-{
-  //code to build the class
-  private int playerId;
-  private String playerName;
-  private String country;
-  private int goals;
-  
-  public Football(int playerId, String playerName, String country, int goals){
-      super();
-      this.playerId=playerId;
-      this.playerName=playerName;
-      this.country=country;
-      this.goals=goals;
-  }
-  //GettersAndSetters
-  
-  public int getplayerId(){
-      return playerId;
-  }
-      public void setplayerId(int playerId){
-          this.playerId=playerId;
-      }
-      public String getplayerName(){
-          return playerName;
-      }
-      public void setplayerName(String playerName){
-          this.playerName=playerName;
-      }
-      public String getcountry(){
-          return country;
-      }
-      public void setcountry(String country){
-          this.country=country;
-      }
-      public int getgoals(){
-          return goals;
-      }
-      public void setgoals(int goals){
-          this.goals=goals;
-      }
-  }
+
+class Book implements Comparable<Book> {
+	private int id;
+	private String title;
+	private String author;
+	private double price;
+
+	public Book() {
+		super();
+	}
+
+	public Book(int id, String title, String author, double price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + "]";
+	}
+
+	@Override
+	public int compareTo(Book b) {
+		// TODO Auto-generated method stub
+		if (this.id != b.id) {
+			return this.id - b.id;
+		}
+		return toString().compareTo(b.toString());
+	}
+
+}
