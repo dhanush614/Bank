d73f88df8f09fcd8e09b1c3dcd0d765b938f8dc1
Dhanush <32243534+dhanush614@users.noreply.github.com>
2020-10-23 12:56:59 +0000

    Update 1602134762442_Solution.java

--- 1602134762442_Solution.java
+++ 1602134762442_Solution.java
@@ -1,105 +1,219 @@
-import java.util.Arrays;
-import java.util.Scanner;
+define(
+	["dojo/_base/declare", "icm/action/Action", "dojo/dom-style",
+		"dijit/form/Button", 'dijit/form/ComboBox',
+		"dojo/_base/declare", "dojo/_base/lang", "dojo/_base/array",
+		"dojo/parser", "dijit/form/ToggleButton", "dojox/grid/cells",
+		"dijit/ToolbarSeparator", "icm/util/Coordination",
+		"dijit/form/Select", "ecm/widget/dialog/BaseDialog",
+		"ecm/widget/FilteringSelect", "dojox/grid/DataGrid",
+		"dojox/grid/cells/dijit", "dojo/data/ItemFileWriteStore",
+		"dijit/dijit", "dojox/layout/TableContainer",
+		"dojox/grid/EnhancedGrid", "dojox/form/uploader/FileList",
+		"dijit/layout/TabContainer", "dijit/layout/ContentPane",
+		"dijit/form/Button", "dijit/layout/LayoutContainer",
+		"pvr/widget/TabContainer", "pvr/widget/Layout",
+		"gridx/modules/Header", "dojo/dom-construct", "dijit/Toolbar",
+		"pvr/widget/PropertyTable", "dojo/dom-class",
+		"gridx/modules/RowHeader", "dojo/data/ObjectStore",
+		"dojo/store/Memory", "gridx/modules/CellWidget",
+		"gridx/modules/dnd/Row", "gridx/modules/Sort",
+		"pvr/widget/editors/grid/GridEditor",
+		"pvr/widget/editors/grid/GridSummary", "dojo/aspect",
+		"dojo/dom-attr", "dojo/request/xhr", "dojo/dom", "dojo/on",
+		"dojo/mouse", "dojo/domReady!"
+	],
+	function(declare, Action, domStyle, Button, ComboBox, declare, lang,
+		array, parser, ToggleButton, cells, ToolbarSeparator,
+		Coordination, Select, BaseDialog, FilteringSelect, DataGrid,
+		cellsDijit, ItemFileWriteStore, dijit, TableContainer,
+		EnhancedGrid, FileList, TabContainer, ContentPane, Button,
+		LayoutContainer, TabContainer, Layout, Header, domConstruct,
+		Toolbar, PropertyTable, domClass, RowHeader, ObjectStore,
+		Memory, CellWidget, Row, Sort, GridEditor, GridSummary, aspect,
+		domAttr, xhr, dom, on, mouse) {
+		return declare(
+			"icmcustom.action.ICMGeneratePropertiesExcelAction",
+			[Action], {
+			solution: null,
+			isEnabled: function() {
+				var Solution = this.getActionContext("Solution");
+				if (Solution === null || Solution.length == 0) {
+					return false;
+				} else {
+					solution = Solution[0];
+					return true;
+				}
+			},
+			execute: function() {
+				this.htmlTemplate = this.buildHtmlTemplate();
+				this.initiateTaskDialog = new BaseDialog({
+					cancelButtonLabel: "Cancel",
+					contentString: this.htmlTemplate,
+					createGrid: function() {
+						var taskLayout = new dijit.layout.TabContainer({
+							cols: 1,
+							spacing: 5,
+							showLabels: true,
+							orientation: "vert"
+						});
 
-public class Solution {
+						var propData = {
+							items: []
+						};
 
-	public static Book[] booksWithPriceRange(double minPrice, double maxPrice, Book[] books) {
-		Book[] result = new Book[2];
-		int j = 0;
-		for (int i = 0; i < books.length; i++) {
-			if (books[i].getPrice() >= minPrice && books[i].getPrice() <= maxPrice) {
-				result[j] = books[i];
-				j++;
-			}
-		}
-		return result;
-	}
+						var propdata_list = solution.attributeDefinitions;
+						var rows = propdata_list.length;
+						for (var i = 0; i < rows; i++) {
+							var propSymbolicName = propdata_list[i].symbolicName;
+							if (propSymbolicName.includes("_")) {
+								var propList = propSymbolicName.split("_");
+								if (propList[0] == "LA") {
+									propData.items.push(propdata_list[i]);
+								}
+							}
+						}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int id;
-		String title, author;
-		double price, minPrice, maxPrice;
-		Book[] books = new Book[4];
-		for (int i = 0; i < 4; i++) {
-			id = sc.nextInt();
-			title = sc.next();
-			author = sc.next();
-			price = sc.nextDouble();
-			books[i] = new Book(id, title, author, price);
-		}
-		minPrice = sc.nextDouble();
-		maxPrice = sc.nextDouble();
-		Arrays.sort(books);
-		Book[] result = booksWithPriceRange(minPrice, maxPrice, books);
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i].getId());
-		}
-	}
-}
+						for (var i = 0; i < propData.items.length; i++) {
+							propData.items[i].id = propData.items[i].name;
+						}
 
-class Book implements Comparable<Book> {
-	private int id;
-	private String title;
-	private String author;
-	private double price;
+						var stateStore = new Memory({
+							data: propData
+						});
+						/*var os = new ObjectStore({
+							objectStore: stateStore
+						});
 
-	public Book() {
-		super();
-	}
+						function formatter() {
+							var select1 = new Select({
+								store: os,
 
-	public Book(int id, String title, String author, double price) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.author = author;
-		this.price = price;
-	}
+							});
+							// select1._destroyOnRemove =
+							// true;
+							return select1;
+						}*/
+						var data = {
+							identifier: "id",
+							items: []
+						};
+						var node = dom.byId("addButton");
+						var i = 0;
+						on(node, "click", function() {
+							var myNewItem = {
+								id: (++i),
+								pname: "",
+								sname: "",
+								isreq: ""
+							};
+							store.newItem(myNewItem);
+						});
+						var remnode = dom.byId("remButton");
+						on(remnode, "click", function() {
+							var items = grid.selection.getSelected();
 
-	public int getId() {
-		return id;
-	}
+							if (items.length) {
+								dojo.forEach(items, function(selectedItem) {
+									if (selectedItem != null) {
+										store.deleteItem(selectedItem);
+										store.save();
+									}
+								})
+							}
+						})
+						layoutProperties = [{
+							defaultCell: {
+								width: 5,
+								editable: false,
+								type: cells._Widget
+							},
+							cells: [
+								new dojox.grid.cells.RowIndex({
+									name: "ID",
+									width: '25px'
+								}),
 
-	public void setId(int id) {
-		this.id = id;
-	}
+								{
+									field: "pname",
+									name: "Property Name",
+									type: dojox.grid.cells._Widget,
+									widgetClass: dijit.form.FilteringSelect,
+									widgetProps: {
+										id: name,
+										store: stateStore,
+										onChange: function() {
+											var store = grid.store;
+											var index = grid.selection.selectedIndex;
+											var item = grid.getItem(index);
+											store.setValue(item, 'sname', this.item.symbolicName);
+											store.setValue(item, 'isreq', this.item.required);
+											grid.update();
+										}
+									},
+									searchAttr: "id",
+									width: '109px',
+									editable: true
+								},
+								{
+									field: "sname",
+									name: "Symbolic Name",
+									width: '109px',
+									height: '109px',
+									editable: false
+								},
+								{
+									field: "isreq",
+									name: "isRequired?",
+									width: '109px',
+									height: '109px',
+									editable: false
+								}
+							]
+						}];
 
-	public String getTitle() {
-		return title;
-	}
+						var store = new ItemFileWriteStore({
+							data: data
+						});
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + "]";
-	}
-
-	@Override
-	public int compareTo(Book b) {
-		// TODO Auto-generated method stub
-		if (this.id != b.id) {
-			return this.id - b.id;
-		}
-		return toString().compareTo(b.toString());
-	}
-
-}
+						var grid = new DataGrid({
+							id: 'grid',
+							store: store,
+							structure: layoutProperties,
+							rowSelector: '20px'
+						});
+						grid.placeAt("gridDiv");
+						grid.startup();
+						// grid.resize({w: '400', h:
+						// '300'});
+					},
+					onExecute: function() {
+						alert('Button clicked');
+					}
+				});
+				this.initiateTaskDialog.setTitle("Properties");
+				this.initiateTaskDialog.createGrid();
+				this.initiateTaskDialog.setSize(500, 500);
+				this.initiateTaskDialog.addButton("Ok",this.initiateTaskDialog.onExecute, false,false);
+				this.initiateTaskDialog.setResizable(true);
+				this.initiateTaskDialog.show();
+			},
+			buildHtmlTemplate: function() {
+				// var dialogueBoxName = "Chose Case Type";
+				var htmlstring = '<div style="width: 500px; height: 300px;"><div data-dojo-type="dijit/layout/TabContainer" style="width: 100%; height: 100%;">' +
+					'<div id="gridDiv" data-dojo-type="dijit/layout/ContentPane" title="Properties" ></div>' +
+					'</div></div>' +
+					'<div class="pvrPropertyTable" id="toolBar1"><div class="pvrPropertyTableGrid" data-dojo-attach-point="_gridNode"></div>' +
+					// '<div id="toolbar1"
+					// data-dojo-type="dijit/Toolbar">'+
+					'<div class="pvrPropertyTableToolbar pvrGridToolbar" data-dojo-type="dijit/Toolbar"  data-dojo-attach-point="_toolbar">' +
+					'<div data-dojo-type="dijit/form/Button" data-dojo-attach-point="_addButton" id="addButton"' +
+					'data-dojo-props="iconClass:\'addButton\', showLabel:false" <!--data-dojo-attach-event="onClick: _onClickAdd"-->>add</div>' +
+					'<div data-dojo-type="dijit/form/Button" data-dojo-attach-point="_removeButton" id="remButton"' +
+					'data-dojo-props="iconClass:\'removeButton\', showLabel:false">remove</div>'
+				// + '</div></div>'
+				'<div class="pvrPropertyTableHidden" data-dojo-attach-point="containerNode"></div' +
+					'></div></div>';
+				return htmlstring;
+			},
+		});
+	});
